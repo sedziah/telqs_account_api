67a7bb9bbd5f26f8bd55a3ba47f9a0763e88d6f6
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2022-02-01 23:39:37 +0000

    Add files via upload

---
+++ api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import *
+
+'''
+default views for jwt
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', UserDetailView.as_view(), name='user-detail'),
+]
+
+'''
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('register/', RegisterAPI.as_view(), name='user-register'),
+    path('login/', LoginAPI.as_view(), name='user-login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('user/', UserDetailView.as_view(), name='user-detail'),
+]
---
+++ api/views.py
@@ -0,0 +1,53 @@
+from api.serializers import *
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': str(refresh.access_token),
+            'refresh_token': str(refresh)
+        })
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': str(refresh.access_token),
+            'refresh_token': str(refresh)
+        })
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
